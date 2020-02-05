721e6437ce9b272b131ce3bfb731ac92340e0a1f
Timothy Robertson <48835868+trryu@users.noreply.github.com>
2020-02-04 21:12:47 -0800

    v1.0
    First build. just the home page with pictures and links that don't work

---
+++ precache-manifest.c62fa2d66b85101f2c648f2737e44ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3614b7b252f08e702087c79447a8c01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b91214e207a905d18f5",
+    "url": "/static/css/main.bd140c4b.chunk.css"
+  },
+  {
+    "revision": "3b56be46e4fbb491496d",
+    "url": "/static/js/2.fabacd8c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.fabacd8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b91214e207a905d18f5",
+    "url": "/static/js/main.ea9b7a56.chunk.js"
+  },
+  {
+    "revision": "6faf262b4024a4b0c855",
+    "url": "/static/js/runtime-main.21fe4d6e.js"
+  }
+]);
